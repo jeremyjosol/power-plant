4a2f828aeeb7419d59d91cb09c18fc314fd93ed9
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-07 11:00:44 -0800

    Add storeState + changeState & begin tests

---
+++ src/js/powerplant.js
@@ -0,0 +1,21 @@
+// stores our state
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+// function factory
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
