09eeb19a7b9f56049d29e4f00854f4ee9eee783d
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-07 11:46:09 -0800

    Pass test for state update twice case

--- __tests__/powerplant.test.js
+++ __tests__/powerplant.test.js
@@ -5,8 +5,16 @@ describe('storeState', () => {
     const stateControl = storeState();
     const feed = changeState("soil")(1);
     const newState = stateControl(feed);
-
     expect(newState).toEqual({ soil : 1 });
+  });
+
+  test('it should correctly store and update when state is changed twice', () => {
+    const stateControl = storeState();
+    const feed = changeState("soil")(1);
+    const compostTea = changeState("soil")(5);
+    stateControl(feed);
+    const newStateWithCompostTea = stateControl(compostTea);
+    expect(newStateWithCompostTea).toEqual({ soil : 6 });
   });
 });
 
